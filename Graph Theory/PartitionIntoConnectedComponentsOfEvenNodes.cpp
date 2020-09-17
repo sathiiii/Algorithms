a1a46ee0a8b51738596351788e98fd795acc7399
Sathira Silva <SathiraSofte@gmail.com>
2020-09-17 09:31:21 +0530

    Create PartitionIntoConnectedComponentsOfEvenNodes.cpp

---
+++ Graph Theory/PartitionIntoConnectedComponentsOfEvenNodes.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dfs(int u, vector<int> graph[], bool visited[], int& count) {
+    visited[u] = true;
+    int currentCount = 0;
+    for (int v : graph[u]) {
+        if (!visited[v]) {
+            int subCount = dfs(v, graph, visited, count);
+            if (subCount % 2 == 0)
+                count++;
+            else
+                currentCount += subCount;
+        }
+    }
+    return currentCount + 1;
+}
+
+int main() {
+    int N, M, n = 0;
+    cin >> N >> M;
+    vector<int> graph[N];
+    map<string, int> m;
+    for (int i = 0; i < M; i++) {
+        string u, v;
+        cin >> u >> v;
+        if (m.find(u) == m.end())
+            m[u] = n++;
+        if (m.find(v) == m.end())
+            m[v] = n++;
+        graph[m.at(u)].push_back(m.at(v));
+        graph[m.at(v)].push_back(m.at(u));
+    }
+    bool visited[N];
+    for (int i = 0; i < N; i++)
+        visited[i] = false;
+    int count = 1;  // Since the graph is given as connected, the graph as a whole is a connected component
+    dfs(0, graph, visited, count);
+    cout << count << endl;
+    return 0;
+}
